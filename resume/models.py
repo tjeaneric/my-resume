6c1cfd17704440183a55dd76291a68fc8828d855
JeanEric <ericjohn415@gmail.com>
2021-08-21 14:55:13 +0200

    updated emailfield

--- resume/models.py
+++ resume/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
     message = models.TextField()
 
     def __str__(self):
