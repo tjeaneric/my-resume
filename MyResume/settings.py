8b1b8c7f478b8e0888ddd5f03b8d48059372998a
JeanEric <ericjohn415@gmail.com>
2021-08-21 08:40:32 +0200

    added

--- MyResume/settings.py
+++ MyResume/settings.py
@@ -141,7 +141,7 @@ BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = "uploaded"
